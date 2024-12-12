a81c12b1e0ac1855b608d316260f5812d4a80f96
Olya Khromova <olya.hrmvlg@gmail.com>
2024-12-12 17:13:56 +0300

    Created Preloader component

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,7 @@
+export default function Preloader() {
+    return (
+        <div className="progress blue-grey lighten-5">
+            <div className="indeterminate blue-grey darken-3"></div>
+        </div>
+    )
+}
