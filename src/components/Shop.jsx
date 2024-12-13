49066284ca40fec2fd4542e35c1f010fd4ffa987
Olya Khromova <olya.hrmvlg@gmail.com>
2024-12-13 12:59:43 +0300

    Added GoodsList component to Shop, implemented logic for displaying goods

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import { API_KEY, API_URL } from '../config';
+import Preloader from './Preloader';
+import GoodsList from "./GoodsList";
+
 export default function Shop() {
+
+    const [goods, setGoods] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(function getGoods() {
+        fetch(API_URL, {
+            headers: {
+                'Authorization': API_KEY,
+            },
+        })
+            .then(response => response.json())
+            .then((data) => {
+                data.shop && setGoods(data.shop);
+                setLoading(false)
+            })
+    }, [])
+
     return (
-        <>
-        </>
+        <div className="container">
+            {
+                isLoading ? (
+                    <Preloader />
+                ) : (
+                    <GoodsList goods={goods} />
+                )
+            }
+        </div>
     )
 }
