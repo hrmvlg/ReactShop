880d9bd49c10daff021d6a238330d9e9bce68a45
Olya Khromova <olya.hrmvlg@gmail.com>
2024-12-13 13:03:20 +0300

    Changed API request language and lang attribute for index.html to ru

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-const API_URL = ' https://fortniteapi.io/v2/shop?lang=en'
+const API_URL = ' https://fortniteapi.io/v2/shop?lang=ru'
 
 export {
     API_KEY,
