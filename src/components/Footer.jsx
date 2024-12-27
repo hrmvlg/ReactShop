262ab5587ecf9a97e909b6fade507c2c7ec791f8
Olya Khromova <olya.hrmvlg@gmail.com>
2024-12-27 16:34:49 +0300

    Update footer and header components to open GitHub repository link in a new tab; adjust footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,8 @@ export default function Footer() {
     return (
         <footer className="page-footer light-blue darken-4">
             <div className="footer-copyright">
-                <div className="container">
-                    © {new Date().getFullYear()} Olya Khromova
-                    <a className="grey-text text-lighten-4 right" href="https://github.com/hrmvlg/ReactShop">GitHub Repository</a>
-                </div>
+                © {new Date().getFullYear()} Olya Khromova
+                <a className="grey-text text-lighten-4 right" href="https://github.com/hrmvlg/ReactShop" target="_blank">GitHub Repository</a>
             </div>
         </footer>
     )
